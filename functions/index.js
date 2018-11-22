4052db6fc680020a8cf58fde7b56967f6508c931
Rick de Hoop <rickdehoop@gmail.com>
2018-11-22 16:31:39 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+const prpl = require('prpl-server');
+const express = require('express');
+const rendertron = require('rendertron-middleware');
+const hsts = require('hsts');
+
+const app = express();
+
+const rendertronMiddleware = rendertron.makeMiddleware({
+  proxyUrl: 'https://render-tron.appspot.com/render',
+  injectShadyDom: true,
+});
+
+app.use(hsts({
+  maxAge: 31536000,
+  includeSubDomains: true,
+  preload: true
+}),(req, res, next) => {
+  req.headers['host'] = 'spero.solutions';
+  return rendertronMiddleware(req, res, next);
+});
+
+app.get('/*', prpl.makeHandler('./build', require('./build/polymer.json')));
+
+exports.app = functions.https.onRequest(app);

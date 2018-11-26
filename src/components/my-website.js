e66b6fdba79bf5b926e9eb03f1f008d9af9f1219
Rick de Hoop <rickdehoop@gmail.com>
2018-11-26 22:58:23 +0100

    updated text for website part

--- src/components/my-website.js
+++ src/components/my-website.js
@@ -25,26 +25,33 @@ class Website extends connect(store)(PageViewElement) {
     return html`
       ${SharedStyles}
       <section>
-        <h2>Technology</h2>
-        <p><code>this</code> application is built with <a href="https://www.polymer-project.org" target="_blank">Polymer WebComponents</a>. The custom elements extend from Polymers <code>&lt;lit-element&gt;</code> which has some nifty functionality regarding rendering. There is a state manager to centralize and manipulate the data. For this we use Redux, you can debug the current state with <a href="http://extension.remotedev.io/" target="_blank">Redux DevTools</a> if interested.</p>
+        <h2>Application</h2>
+        <p><code>this</code> application is built with <a href="https://www.polymer-project.org" target="_blank">Polymer WebComponents</a>. The custom elements extend from Polymers <code>&lt;lit-element&gt;</code> which has some nifty functionality regarding rendering. There is a state manager to centralize and manipulate the data, this is done with Redux. You can debug the current state with <a href="http://extension.remotedev.io/" target="_blank">Redux DevTools</a> if interested.</p>
       </section>
       <section>
-        <h3>Welcome</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac nisi orci. Maecenas sollicitudin diam in diam efficitur cursus. Morbi sollicitudin in justo tincidunt placerat. Integer tincidunt elementum nisi, eu ornare dolor lacinia eget. Fusce pulvinar massa eget odio placerat, commodo molestie ipsum tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse porttitor id purus eu cursus. Suspendisse arcu nulla, mattis vel hendrerit et, malesuada a elit. Nam at diam ornare, aliquet est sed, malesuada metus. Cras nec enim vel nibh tincidunt euismod ut et enim. Etiam pharetra eros in sodales iaculis. Duis sagittis urna et cursus mollis. Cras tempor rutrum est. Praesent sollicitudin ligula at laoreet placerat. Praesent tortor dui, semper in sapien non, pharetra luctus turpis.</p>
+        <h3>Quality</h3>
+        <p>The quality of this application is guaranteed, as far as possible, by automatic tests and performance audits.</p>
+        <h4>Testing</h4>
+        <p>For unit testing <a href="https://github.com/Polymer/tools/tree/master/packages/web-component-tester" target="_blank">WebComponentTester</a> (WCT) is used. WCT is a tool which enables you to run your tests in different browsers while still be able to use popular testing combinations like Mocha and Chai. <code>a11ySuite</code> is included for accessibility checks.</p>
+        <p>The router, and some visuals, are tested with <a href="https://developers.google.com/web/tools/puppeteer/" target="_blank">Puppeteer</a>. This library gives you direct access to <a href="https://developers.google.com/web/updates/2017/04/headless-chrome" target="_blank">headless</a> Chrome. This tool enables you to interact with Chrome via NodeJS as if you are an end user.</p>
+        <p>Those tests will run on every commit via <a href="https://travis-ci.com/iceicerickert/spero-solutions-website" target="_blank">Travis CI</a>.</p>
+        <h4>Performance and standards</h4>
+        <p><a href="https://www.webpagetest.org/easy" target="_blank">Webpagetest</a> does some performance testing on a real Moto G4 device on a slow 3G network. It gives you some insights on how the page performs.</p>
+        <p><a href="https://www.webpagetest.org/result/181126_RR_e130ae40a2aa84613167d0b9d72e8c01/" target="_blank">Click here</a> to view the results of this page. You can click re-run to have give it another go! <a href="https://developers.google.com/web/tools/lighthouse/" target="_blank">Lighouse</a> audits are also done on production environment. They can be viewed from the same link or from <a href="https://www.webpagetest.org/lighthouse.php?test=181126_RR_e130ae40a2aa84613167d0b9d72e8c01&run=2" target="_blank">here</a>.</p>
+        <p>Some other checks or audits of this page: <a href="https://hstspreload.org/?domain=spero.solutions" target="_blank">HSTS</a>, <a href="https://www.ssllabs.com/ssltest/analyze.html?d=spero.solutions&latest" target="_blank">SSL Labs</a>, <a href="https://webhint.io/#scanner-input" target="_blank">webhint</a></p>
       </section>
       <section>
-        <p>Vestibulum at est ex. Aenean id ligula id nibh dictum laoreet. Etiam non semper erat. Pellentesque eu justo rhoncus diam vulputate facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat metus ex, vel fringilla massa tincidunt sit amet. Nunc facilisis bibendum tristique. Mauris commodo, dolor vitae dapibus fermentum, odio nibh viverra lorem, eu cursus diam turpis et sapien. Nunc suscipit tortor a ligula tincidunt, id hendrerit tellus sollicitudin.</p>
+        <h3>Deployment</h3>
+        <p>The source code is managed by a GitHub repository, <a href="https://github.com/iceicerickert/spero-solutions-website" target="_blank">iceicerickert/spero-solutions-website</a>. A deployment is triggered on ervery commit to the master branch (IF the tests, audits and build are succeeding). Travis will then deploy the application to <a href="http://firebase.google.com" target=_blank">Firebase</a>. The application server (<a href="https://github.com/Polymer/prpl-server" target="_blank">prpl-server-node</a>) is deployed as a Firebase function. The static files are hosted by the hosting service of Firebase. This gives you the option to serve browser specific builds (ESM, ES5, ES6) for the best experience.</p>
+        <p>Next to that, everything is server over HTTPS.</p>
       </section>
-      <section>
-        <h2>Redux example: simple counter</h2>
-        <div class="circle">${this._value}</div>
-        <p>This page contains a reusable <code>&lt;counter-element&gt;</code>. The
-        element is not built in a Redux-y way (you can think of it as being a
-        third-party element you got from someone else), but this page is connected to the
-        Redux store. When the element updates its counter, this page updates the values
-        in the Redux store, and you can see the current value of the counter reflected in
-        the bubble above.</p>
-        <br><br>
+        <section>
+        <h3>Some nice tricks..</h3>
+        <ul>
+          <li>Switch of your internet connection and see how you can still use this page because of the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API" target="_blank">ServiceWorker API</a>.</li>
+          <li>Check your network requests and see the number of roundtrips have been reduced due to <a href="https://developers.google.com/web/fundamentals/performance/http2/" target="_blank">HTTP/2</a>.</li>
+          <li>Add this <a href="https://developers.google.com/web/progressive-web-apps/" target="_blank">Progressive Web App</a> (PWA) to your homescreen and it feels like a native app!</li>
+        </ul>
       </section>
       <section>
         <p>

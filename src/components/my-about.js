36fad3af8ab662f0438c65afd7c66bc448964ded
Rick de Hoop <rickdehoop@gmail.com>
2019-01-28 13:15:43 +0100

    updated about + polymer

--- src/components/my-about.js
+++ src/components/my-about.js
@@ -10,7 +10,9 @@ class About extends PageViewElement {
       ${SharedStyles}
       <section>
         <h2>About me</h2>
-        <p>This page will be extended in the near future.</p>
+        <p>Software Engineer specialised in Javascript. Both frontend oriented and backend oriented. Frontend: worked with webcomponents, PWA and the popular JS frameworks. Backend: NodeJS</p>
+        <p>Test Engineer specialised in test automation setup. From checks in the code editor and pull request until tool selection, setup and implementation for unit and integration tests.</p>
+        <p>Team Builder, Goal Oriented and Quality Focused.</p>
       </section>
     `;
   }
